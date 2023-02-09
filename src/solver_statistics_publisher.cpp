2d0b8418e5b687ebaddccb8690bfc08076ed2bef
Carlos Mastalli <c.mastalli@hw.ac.uk>
2023-02-09 23:37:03 +0000

    [solver] Added documentation and standarized message

--- src/solver_statistics_publisher.cpp
+++ src/solver_statistics_publisher.cpp
@@ -14,6 +14,11 @@
 
 class SolverStatisticsRosPublisher {
 public:
+  /**
+   * @brief Initialize the solver statistic publisher
+   *
+   * @param[in] topic  Topic name
+   */
   SolverStatisticsRosPublisher(
       const std::string &topic = "/crocoddyl/solver_statistics") {
     ros::NodeHandle n;
@@ -22,23 +27,35 @@ public:
   }
   ~SolverStatisticsRosPublisher() = default;
 
-  void publish(const std::size_t iterations, const double total_time,
-               const double solve_time, const double cost,
-               const double regularization, const double step_length,
-               const double dynamic_feasibility,
-               const double equality_infeasibility,
-               const double inequality_infeasibility) {
+  /**
+   * @brief Publish a solver statistic ROS message
+   *
+   * @param iterations[in]      Number of solver iterations
+   * @param totaltime[in]       Total time
+   * @param solvetime[in]       Solving time
+   * @param cost[in]            Total cost
+   * @param regularization[in]  Regularization value
+   * @param steplength[in]      Step length applied by the solver
+   * @param dynfeas[in]         Dynamic feasibility
+   * @param equafeas[in]        Equality constraints feasibility
+   * @param ineqfeas[in]        Inequality constraints feasibility
+   */
+  void publish(const std::size_t iterations, const double totaltime,
+               const double solvetime, const double cost,
+               const double regularization, const double steplength,
+               const double dynfeas, const double equafeas,
+               const double ineqfeas) {
     if (pub_.trylock()) {
       pub_.msg_.stamp = ros::Time::now();
       pub_.msg_.iterations = iterations;
-      pub_.msg_.total_time = total_time;
-      pub_.msg_.solve_time = solve_time;
+      pub_.msg_.total_time = totaltime;
+      pub_.msg_.solve_time = solvetime;
       pub_.msg_.cost = cost;
       pub_.msg_.regularization = regularization;
-      pub_.msg_.step_length = step_length;
-      pub_.msg_.dynamic_feasibility = dynamic_feasibility;
-      pub_.msg_.equality_infeasibility = equality_infeasibility;
-      pub_.msg_.inequality_infeasibility = inequality_infeasibility;
+      pub_.msg_.step_length = steplength;
+      pub_.msg_.dynamic_feasibility = dynfeas;
+      pub_.msg_.equality_feasibility = equafeas;
+      pub_.msg_.inequality_feasibility = ineqfeas;
 
       pub_.unlockAndPublish();
     }
@@ -65,9 +82,20 @@ PYBIND11_MODULE(solver_statistics_ros_publisher_py, m) {
       .def(py::init<const std::string &>(),
            py::arg("topic_mpc_statistics") = "/crocoddyl/solver_statisticss")
       .def("publish", &SolverStatisticsRosPublisher::publish,
+           "Publish a solver statistic ROS message.\n\n"
+           ":param iterations: number of solver iterations\n"
+           ":param total_time: total computation time\n"
+           ":param solve_time: solving time\n"
+           ":param cost: total cost\n"
+           ":param regularization: regularization value\n"
+           ":param step_length: step length applied by the solver\n"
+           ":param dynamic_feasibility: dynamic feasibility\n"
+           ":param equality_feasibility: equality constraints feasibility "
+           "(default 0)\n"
+           ":param inequality_feasibility: inequality constraints feasibility "
+           "(default 0)",
            py::arg("iterations"), py::arg("total_time"), py::arg("solve_time"),
            py::arg("cost"), py::arg("regularization"), py::arg("step_length"),
-           py::arg("dynamic_feasibility"),
-           py::arg("equality_infeasibility") = 0.,
-           py::arg("inequality_infeasibility") = 0.);
+           py::arg("dynamic_feasibility"), py::arg("equality_feasibility") = 0.,
+           py::arg("inequality_feasibility") = 0.);
 }
